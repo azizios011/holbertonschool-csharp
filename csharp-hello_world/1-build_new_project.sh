e8c440a39ff74c39633218b301ace39dd29f92fa
azizios011 <hdriaziz699@gmail.com>
2023-12-06 20:16:03 +0100

    Build new project

---
+++ csharp-hello_world/1-build_new_project.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Create a directory for the new project
+mkdir 1-new_project
+
+# Navigate into the new project directory
+cd 1-new_project || exit
+
+# Initialize a new C# console application using dotnet
+dotnet new console
+
+# Display a success message
+echo "The template \"Console Application\" was created successfully."
+
+dotnet build
+
+# Run 'dotnet restore' to restore project dependencies
+dotnet restore
