16c448a79759927ac7286a7211dde94bc61feb39
azizios011 <hdriaziz699@gmail.com>
2023-12-07 01:42:33 +0100

    Hello, WriteLine

--- csharp-hello_world/3-writeline/3-writeline.cs
+++ csharp-hello_world/3-writeline/3-writeline.cs
@@ -1,2 +1,10 @@
 ﻿// a C# script that prints exactly "Programming is like building a multilingual puzzle followed by a new line.
-Console.WriteLine("\"Programming is like building a multilingual puzzle");
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("\"Programming is like building a multilingual puzzle");
+    }
+}
