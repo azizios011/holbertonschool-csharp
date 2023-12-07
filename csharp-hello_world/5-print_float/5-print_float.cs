66978b9933748653199cdf057f9a618e17daeebe
azizios011 <hdriaziz699@gmail.com>
2023-12-07 06:55:06 +0100

    Print float

---
+++ csharp-hello_world/5-print_float/5-print_float.cs
@@ -0,0 +1,12 @@
+﻿// a program that print the float stored in the variable number with a precision of 2 digits.
+using System;
+
+class Program
+{
+	static void Main(string[] args)
+        {
+		float number = 3.14159f;
+        // Display float with two decimal places
+		Console.WriteLine($"Float: {number:F2}");
+        }
+}
