f2231bfaa72be92f76f22022a1472466ddeba242
azizios011 <hdriaziz699@gmail.com>
2023-12-07 07:52:21 +0100

    Play with strings

---
+++ csharp-hello_world/7-concat/7-concat.cs
@@ -0,0 +1,12 @@
+﻿// a program that conact 2 strings and print Welcome to Holberton School!.
+using System;
+class Program
+{
+	static void Main(string[] args)
+	{
+		string str1 = "Holberton";
+		string str2 = "School";
+        str1 = str1 += str2;
+		Console.WriteLine("Welcome to {0}!", str1);
+	}
+}
