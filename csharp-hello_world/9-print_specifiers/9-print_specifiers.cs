e8ab60b59d42755cd4165d5883d6dccf278dafec
azizios011 <hdriaziz699@gmail.com>
2023-12-07 08:43:00 +0100

    Print specifiers

---
+++ csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -0,0 +1,12 @@
+﻿// a program that print the given variables as in the result shown.
+using System.Globalization;
+class Program
+{
+	static void Main(string[] args)
+	{
+		double percent = .7553;
+		double currency = 98765.4321;
+		Console.WriteLine($"Percent: {(percent * 100):F2}%");
+        Console.WriteLine($"Currency: ${currency.ToString("N2", CultureInfo.InvariantCulture)}");
+	}
+}
