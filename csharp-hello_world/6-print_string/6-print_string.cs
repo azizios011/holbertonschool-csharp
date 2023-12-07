450ebc6de523408f6f4b3493e8582c366ccb2629
azizios011 <hdriaziz699@gmail.com>
2023-12-07 07:24:36 +0100

    Print string

---
+++ csharp-hello_world/6-print_string/6-print_string.cs
@@ -0,0 +1,12 @@
+﻿﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		string str = "Holberton School";
+		Console.WriteLine($"{str}{str}{str}");
+        Console.WriteLine();
+        Console.WriteLine(str.Substring(0, Math.Min(9, str.Length)));
+        }
+}
