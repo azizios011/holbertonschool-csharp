6c82d6c08f302f224c6447f82b8997ddf8f540cc
azizios011 <hdriaziz699@gmail.com>
2023-12-10 18:09:51 +0100

    islower

---
+++ csharp-ifelse_loops_methods/7-islower/7-islower.cs
@@ -0,0 +1,11 @@
+﻿// a method that checks for lowercase character.
+using System;
+
+class Character
+{
+    public static bool IsLower(char c)
+    {
+        // ASCII values for lowercase letters: 'a' to 'z' are from 97 to 122
+        return c >= 'a' && c <= 'z';
+    }
+}
