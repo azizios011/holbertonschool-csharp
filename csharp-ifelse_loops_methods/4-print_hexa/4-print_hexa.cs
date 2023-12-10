1dce91edf4d70c3054136372d2ef56986cb84222
azizios011 <hdriaziz699@gmail.com>
2023-12-10 17:44:00 +0100

    Hexadecimal printing

--- csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
+++ csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -1,12 +1,17 @@
-﻿using System;
+﻿// a program that prints all numbers from 0 to 98 in decimal and in hexadecimal.
+using System;
 
-class Program
+namespace _4_print_hexa
 {
-    static void Main(string[] args)
+    class Program
     {
-        for (int i = 0; i <= 98; i++)
+        static void Main(string[] args)
         {
-            Console.Write($"{i} = 0x{i:X}\n");
+            for (int i = 0; i < 99; i++)
+            {
+                string hex = i.ToString("x");
+                Console.Write($"{i} = 0x{hex}\n");
+            }
         }
     }
 }
