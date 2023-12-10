b65b4773edaeaeaf014fd8003e49dabe1df4cd26
azizios011 <hdriaziz699@gmail.com>
2023-12-10 18:15:24 +0100

    a + b

---
+++ csharp-ifelse_loops_methods/9-add/9-add.cs
@@ -0,0 +1,11 @@
+﻿// a method that adds two integers and returns the result.
+using System;
+
+class Number
+{
+    public static int Add(int a, int b)
+    {
+        int sum = a + b;
+        return sum;
+    }
+}
