fa06eb53321e113d8fa81eb73d3638cc030ae975
azizios011 <hdriaziz699@gmail.com>
2023-12-10 18:13:07 +0100

    There are only 3 colors, 10 digits, and 7 notes; its what we do with them that s important

---
+++ csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -0,0 +1,12 @@
+﻿// a method that prints the last digit of a number.
+using System;
+
+class Number
+{
+    public static int PrintLastDigit(int number)
+    {
+        int lastDigit = Math.Abs(number % 10);
+        Console.Write(lastDigit);
+        return lastDigit;
+    }
+}
