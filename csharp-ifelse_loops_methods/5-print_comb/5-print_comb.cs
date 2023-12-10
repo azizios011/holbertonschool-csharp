a2f663d1d83334a292a0f31da7bbd6ebb377f3b3
azizios011 <hdriaziz699@gmail.com>
2023-12-10 17:56:16 +0100

    00...99

---
+++ csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for(int i = 0; i <= 99; i++)
+        {
+            Console.Write($"{i:D2}");
+            if (i < 99)
+            {
+                Console.Write(", ");
+            }
+            else
+            {
+                Console.Write("\n");
+            }
+        }
+    }
+}
