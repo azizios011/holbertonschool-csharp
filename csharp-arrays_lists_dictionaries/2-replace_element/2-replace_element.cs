c3a106c3888b85ac93b4aa18416a0f682a86c84c
azizios011 <hdriaziz699@gmail.com>
2023-12-16 11:29:57 +0100

    Replace element

---
+++ csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
@@ -0,0 +1,19 @@
+﻿// a method that replaces an element of an array at a given index.
+using System;
+
+class Array
+{
+    public static int[] ReplaceElement(int[] array, int index, int n)
+    {
+        if (index < 0 || index >= array.Length)
+        {
+            Console.WriteLine("index out of range");
+            return array;
+        }
+        else
+        {
+            array[index] = n;
+            return array;
+        }
+    }
+}
