cc0fe69ca9a64c2a9d9baf21a258849c9505a1e5
azizios011 <hdriaziz699@gmail.com>
2023-12-16 20:22:48 +0100

    Delete at

--- csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
+++ csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
@@ -1,19 +1,15 @@
 ﻿// a method that deletes the item at a specific position in a list.
-using System;
+﻿using System;
 using System.Collections.Generic;
 
-class List
-{
-    public static List<int> DeleteAt(List<int> myList, int index)
+    class List
     {
-        if (index < 0 || index >= myList.Count)
+        public static List<int> DeleteAt(List<int> myList, int index)
         {
-            Console.WriteLine("Index is out of range");
-        }
-        else
-        {
-            myList.Remove(myList[index]);
+            if (index < 0 || index >= myList.Count)
+                Console.WriteLine("Index is out of range");
+            else
+                myList.Remove(myList[index]);
             return myList;
         }
     }
-}
