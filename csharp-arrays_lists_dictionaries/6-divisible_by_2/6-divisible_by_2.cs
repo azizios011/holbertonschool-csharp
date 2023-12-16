9980cc99cf9d3d47a02066ccea28ba06b4a9bd34
azizios011 <hdriaziz699@gmail.com>
2023-12-16 19:13:29 +0100

    Only by 2

---
+++ csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -0,0 +1,24 @@
+﻿// a method that finds all multiples of 2 in a list.
+using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<bool> DivisibleBy2(List<int> myList)
+    {
+        if (myList == null)
+        {
+            Console.WriteLine("List is null");
+            return null;
+        }
+
+        List<bool> result = new List<bool>();
+
+        foreach (int number in myList)
+        {
+            result.Add(number % 2 == 0);
+        }
+
+        return result;
+    }
+}
