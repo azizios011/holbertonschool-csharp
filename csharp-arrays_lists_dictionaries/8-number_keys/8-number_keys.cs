8fb6996421bdf0bf3417789342d30477c126530b
azizios011 <hdriaziz699@gmail.com>
2023-12-17 23:38:42 +0100

    Number of keys

---
+++ csharp-arrays_lists_dictionaries/8-number_keys/8-number_keys.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static int NumberOfKeys(Dictionary<string, string> myDict)
+    {
+        if (myDict == null)
+        {
+            Console.WriteLine("Dictionary is null");
+            return 0;
+        }
+
+        int count = 0;
+
+        foreach (var key in myDict.Keys)
+        {
+            count++;
+        }
+
+        return count;
+    }
+}
