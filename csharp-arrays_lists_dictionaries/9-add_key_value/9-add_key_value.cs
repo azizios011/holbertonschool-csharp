145b4f106427ae698845cd5644ff25736ceba064
azizios011 <hdriaziz699@gmail.com>
2023-12-18 19:44:38 +0100

    Update dictionary

---
+++ csharp-arrays_lists_dictionaries/9-add_key_value/9-add_key_value.cs
@@ -0,0 +1,24 @@
+﻿// a method that adds a key and value to a dictionary.
+using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static Dictionary<string, string> AddKeyValue(Dictionary<string, string> myDict, string key, string value)
+    {
+        if (myDict == null)
+        {
+            Console.WriteLine("Dictionary is null");
+            return null;
+        }
+        else if (myDict.ContainsKey(key))
+        {
+            myDict[key] = value;
+        }
+        else
+        {
+            myDict.Add(key, value);
+        }
+        return myDict;
+    }
+}
