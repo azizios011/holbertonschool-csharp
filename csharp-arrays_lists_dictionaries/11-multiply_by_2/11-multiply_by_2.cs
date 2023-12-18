bed910c608276ba9ad17ddc44a15846518fca152
azizios011 <hdriaziz699@gmail.com>
2023-12-18 20:24:45 +0100

    Multiply by 2

---
+++ csharp-arrays_lists_dictionaries/11-multiply_by_2/11-multiply_by_2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static Dictionary<string, int> MultiplyBy2(Dictionary<string, int> myDict)
+    {
+        if (myDict == null)
+        {
+            Console.WriteLine("Dictionary is null");
+            return null;
+        }
+        foreach (var kvp in myDict)
+        {
+            myDict[kvp.Key] = kvp.Value * 2;
+        }
+        return myDict;
+    }
+}
