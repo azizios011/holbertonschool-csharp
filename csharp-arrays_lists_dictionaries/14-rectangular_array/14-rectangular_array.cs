7d96c749fe71f1b05f3c7e91b6b566237aaf73d3
azizios011 <hdriaziz699@gmail.com>
2023-12-22 02:29:15 +0100

    Rectangular array

---
+++ csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,26 @@
+﻿// a program that creates and prints a 5 by 5 two-dimensional array and initialize index [2,2] to 1 and all other indices to 0.
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[,] array = new int[5, 5];
+
+        array[2, 2] = 1;
+
+        Print2DArray(array);
+    }
+
+    static void Print2DArray(int[,] array)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                Console.Write(array[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
