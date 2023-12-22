270c164e2bf7bbfcc29e4a516e0ca333a17b3e4c
azizios011 <hdriaziz699@gmail.com>
2023-12-22 02:43:41 +0100

    Jagged array

---
+++ csharp-arrays_lists_dictionaries/100-jagged_array/100-jagged_array.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[][] jaggedArray = new int[][]
+        {
+            new int[] {0, 1, 2, 3},
+            new int[] {0, 1, 2, 3, 4, 5, 6},
+            new int[] {0, 1}
+        };
+
+        PrintJaggedArray(jaggedArray);
+    }
+
+    static void PrintJaggedArray(int[][] jaggedArray)
+    {
+        foreach (var array in jaggedArray)
+        {
+            Console.WriteLine(string.Join(" ", array));
+        }
+    }
+}
