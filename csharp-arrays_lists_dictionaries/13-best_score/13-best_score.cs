8d4e883638518f286c54ea19a24ffe13525dec12
azizios011 <hdriaziz699@gmail.com>
2023-12-22 02:19:11 +0100

    Best score

---
+++ csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,29 @@
+﻿// Write a method that returns the key with the biggest integer value in a given dictionary.
+using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        if (myList == null || myList.Count == 0)
+        {
+            Console.WriteLine("Dictionary is null or empty");
+            return "None";
+        }
+
+        int maxScore = int.MinValue;
+        string bestStudent = "None";
+
+        foreach (var kvp in myList)
+        {
+            if (kvp.Value > maxScore)
+            {
+                maxScore = kvp.Value;
+                bestStudent = kvp.Key;
+            }
+        }
+
+        return bestStudent;
+    }
+}
