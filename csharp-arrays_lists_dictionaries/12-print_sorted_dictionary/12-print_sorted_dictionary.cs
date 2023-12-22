abadd0cf251bc2cc25b7f62f55896d9fd0ec4e16
azizios011 <hdriaziz699@gmail.com>
2023-12-22 02:12:39 +0100

    Print sorted dictionary

---
+++ csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -0,0 +1,16 @@
+﻿// a method that prints a dictionary by ordered keys.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Dictionary
+{
+    public static void PrintSorted(Dictionary<string, string> myDict)
+    {
+        var new_dict = myDict.OrderBy(x => x.Key);
+        foreach (var element in new_dict)
+        {
+            Console.WriteLine($"{element.Key}: {element.Value}");
+        }
+    }
+}
