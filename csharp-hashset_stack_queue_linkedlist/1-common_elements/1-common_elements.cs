9d8c4fa4e09d75fce17fcea00597ed610a9a0d5b
azizios011 <hdriaziz699@gmail.com>
2023-12-29 09:18:38 +0100

    Present in both

---
+++ csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CommonElements(List<int> list1, List<int> list2)
+    {
+        HashSet<int> uniqueElements = new HashSet<int> (list1);
+        List<int> commonElements = new List<int> ();
+
+        foreach (var item in list2)
+        {
+            if (uniqueElements.Contains(item))
+            {
+                commonElements.Add(item);
+                uniqueElements.Remove(item);
+            }
+        }
+        commonElements.Sort();
+        return commonElements;
+    }
+}
