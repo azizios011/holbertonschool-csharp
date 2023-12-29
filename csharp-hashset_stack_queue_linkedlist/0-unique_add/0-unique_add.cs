6c102213a5f42a512a2049ef473af31b57058972
azizios011 <hdriaziz699@gmail.com>
2023-12-29 08:19:07 +0100

    Unique addition

---
+++ csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int Sum(List<int> myList)
+    {
+        HashSet<int> myHashSet = new HashSet<int>();
+        myHashSet.UnionWith(myList);
+        
+        int sum = 0;
+        foreach ( var item in myHashSet)
+        {
+            sum += item;
+        }
+        return sum;
+    }
+}
