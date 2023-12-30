dbe8e328fdc9b11b48f5785d05427d49b8c1d6e5
azizios011 <hdriaziz699@gmail.com>
2023-12-30 06:19:24 +0100

    Full stack

---
+++ csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,46 @@
+﻿// a method that check a stack for it's info.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class MyStack
+{
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+    {
+        Console.WriteLine($"Number of items: {aStack.Count}");
+
+        if (aStack.Count > 0)
+        {
+            string topItem = aStack.Peek();
+            Console.WriteLine($"Top item: {topItem}");
+
+            bool containsSearch = aStack.Contains(search);
+            Console.WriteLine($"Stack contains {search}: {containsSearch}");
+
+            if (containsSearch)
+            {
+                Stack<string> tempStack = new Stack<string>();
+                while (aStack.Count > 0 && aStack.Peek() != search)
+                {
+                    tempStack.Push(aStack.Pop());
+                }
+                if (aStack.Count > 0)
+                {
+                    aStack.Pop();
+                }
+                while (tempStack.Count > 0)
+                {
+                    aStack.Push(tempStack.Pop());
+                }
+            }
+        }
+        else
+        {
+            Console.WriteLine("Stack is empty");
+        }
+
+        aStack.Push(newItem);
+
+        return aStack;
+    }
+}
