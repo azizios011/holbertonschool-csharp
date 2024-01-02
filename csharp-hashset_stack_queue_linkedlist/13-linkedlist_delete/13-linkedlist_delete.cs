e91e073348c9d982fa65310564b9cae75fd6be56
azizios011 <hdriaziz699@gmail.com>
2024-01-02 08:26:40 +0100

    Delete at index

---
+++ csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -0,0 +1,23 @@
+﻿// a method that deletes the node at given position in a LinkedList.
+using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static void Delete(LinkedList<int> myLList, int index)
+    {
+        if (index < 0 || index >= myLList.Count)
+        {
+            Console.WriteLine("Invalid index");
+            return;
+        }
+
+        LinkedListNode<int> currentNode = myLList.First;
+        for (int i = 0; i < index; i++)
+        {
+            currentNode =currentNode.Next;
+        }
+
+        myLList.Remove(currentNode);
+    }
+}
