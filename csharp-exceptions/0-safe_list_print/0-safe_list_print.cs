54cf5b9e4c6ba9d5764275b7b8b1ae5eecc40143
azizios011 <hdriaziz699@gmail.com>
2024-01-15 23:48:35 +0100

    Safe list printing

---
+++ csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int SafePrint(List<int> myList, int n)
+    {
+        try
+        {
+            int count = 0;
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(myList[i]);
+                count++;
+            }
+            return count;
+        }
+
+        catch (ArgumentOutOfRangeException)
+        {
+            Console.WriteLine("Warning: Attempted to print more elements than available in the list.");
+            return myList.Count;
+        }
+    }
+}
