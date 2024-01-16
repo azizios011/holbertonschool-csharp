664219b6c2f21f5907dfcb746563e61af5792222
azizios011 <hdriaziz699@gmail.com>
2024-01-16 02:08:45 +0100

    Integer division

---
+++ csharp-exceptions/1-divide_print/1-divide_print.cs
@@ -0,0 +1,25 @@
+﻿// a function that divides 2 integers and prints the result.
+using System;
+using System.Collections.Generic;
+
+class Int
+{
+    public static void divide(int a, int b)
+    {
+        try
+        {
+            int result = a / b;
+            Console.WriteLine($"{a} / {b} = {result}");
+        }
+
+        catch (DivideByZeroException)
+        {
+           Console.WriteLine($"Cannot divide by zero");
+        }
+
+        finally
+        {
+            Console.WriteLine($"{a} / {b} = {(b != 0 ? a / b : 0)}");
+        }
+    }
+}
