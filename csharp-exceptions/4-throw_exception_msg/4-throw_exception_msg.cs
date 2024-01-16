34a6b3d6bc0dd5ea4bcc574b4280418599a91050
azizios011 <hdriaziz699@gmail.com>
2024-01-16 05:31:53 +0100

    Throw exception with message

---
+++ csharp-exceptions/4-throw_exception_msg/4-throw_exception_msg.cs
@@ -0,0 +1,10 @@
+﻿// a method that throws an Exception with a message.
+using System;
+
+class Except
+{
+    public static void ThrowMsg(string Msg)
+    {
+        throw new Exception(Msg);
+    }
+}
