71234b429df225849997fec10da46dd4c7fac4a8
azizios011 <hdriaziz699@gmail.com>
2024-01-16 05:23:11 +0100

    Throw exception

---
+++ csharp-exceptions/3-throw_exception/3-throw_exception.cs
@@ -0,0 +1,11 @@
+﻿// a function that throws an Exception.
+using System;
+using System.Collections.Generic;
+
+class Except
+{
+    public static void Throw()
+    {
+        throw new Exception();
+    }
+}
