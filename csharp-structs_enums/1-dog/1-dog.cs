db4395ae46ec4932f14a874c3f24930ac40695ae
azizios011 <hdriaziz699@gmail.com>
2024-01-19 04:49:08 +0100

    Chief Puppy Officer

---
+++ csharp-structs_enums/1-dog/1-dog.cs
@@ -0,0 +1,16 @@
+﻿// a new struct 'Dog' with members'name, age, owner, rating'.
+using System;
+
+enum Rating
+{
+    Good,
+    Great,
+    Excellent
+}
+struct Dog
+{
+    public string name;
+    public float age;
+    public string owner;
+    public Rating rating;
+}
