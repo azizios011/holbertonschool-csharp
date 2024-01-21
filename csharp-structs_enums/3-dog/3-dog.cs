1df4ec5941b77f8507fed9f27f0280bcdb05d4c8
azizios011 <hdriaziz699@gmail.com>
2024-01-21 03:10:49 +0100

    A dog will teach you unconditional love. If you can have that in your life, things won t be too bad

---
+++ csharp-structs_enums/3-dog/3-dog.cs
@@ -0,0 +1,29 @@
+﻿// Override the '.ToString()' method to print the Dog object’s attributes to stdout.
+using System;
+
+enum Rating
+{
+    Good,
+    Great,
+    Excellent
+}
+struct Dog
+{
+    public string name;
+    public float age;
+    public string owner;
+    public Rating rating;
+
+    public Dog(string name, float age, string owner, Rating rating)
+    {
+        this.name = name;
+        this.age = age;
+        this.owner = owner;
+        this.rating = rating;
+    }
+
+    public override string ToString()
+    {
+        return $"Dog Name: {name}\nAge: {age}\nOwner: {owner}\nRating: {rating}";
+    }
+}
