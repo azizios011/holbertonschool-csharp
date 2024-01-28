801ac709ed97166853f262f5bb333ba9ca466621
azizios011 <hdriaziz699@gmail.com>
2024-01-28 19:06:11 +0100

    Healthy competition

---
+++ csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+
+namespace Enemies
+{
+    /// <summary>
+    /// Represents an empty public class 'Zombie'.
+    /// </summary>
+    public class Zombie
+    {
+        /// <summary>
+        /// Get or sets the health of the Zombie.
+        /// </summary>
+        public int health { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cerf="Zombie"/> class.
+        /// </summary>
+        public Zombie()
+        {
+            // Set the intial value of health to 0 in the constructor.
+            health = 0;
+        }
+    }
+}
