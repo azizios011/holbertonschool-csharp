dbd6e8310b23f1e5bf0eb86806cf27f8413985fc
azizios011 <hdriaziz699@gmail.com>
2024-01-28 18:49:23 +0100

    Braaainsss

--- csharp-classes/0-enemy/0-enemy.cs
+++ csharp-classes/0-enemy/0-enemy.cs
@@ -1,15 +1,12 @@
 ﻿using System.ComponentModel;
 
-/// <summary>
-/// a new namespace 'Enemies'
-/// </summary>
 namespace Enemies
 {
     /// <summary>
-    /// an empty public class 'Zombie'.
+    /// Represents an empty public class 'Zombie'.
     /// </summary>
     public class Zombie
     {
-    
+        // No constructor in the original code
     }
 }
