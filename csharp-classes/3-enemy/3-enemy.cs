ec4a9591d023cc986560cc79e0dfca7ddc31d498
azizios011 <hdriaziz699@gmail.com>
2024-02-01 19:31:56 +0100

    Zombie health

---
+++ csharp-classes/3-enemy/3-enemy.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Enemies
+{
+    /// <summary>
+    /// Represents an empty public class 'Zombie'.
+    /// </summary>
+    public class Zombie
+    {
+        /// <summary>
+        /// Gets or sets the health of the Zombie.
+        /// </summary>
+        private int health { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Zombie"/> class.
+        /// </summary>
+        public Zombie()
+        {
+            // Set the initial value of health to 0 in the constructor.
+            health = 0;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Zombie"/> class with the specified health value.
+        /// </summary>
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+
+            // Set the health value in the constructor.
+            health = value;
+        }
+
+        /// <summary>
+        /// Public method 'GetHealth' that returns the value of health of the Zombie object.
+        /// </summary>
+        public int GetHealth()
+        {
+            int result = health;
+            return result;
+        }
+    }
+}
