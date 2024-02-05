cc26186f692554852eec6b5fb23bbb7f151b1ef9
azizios011 <hdriaziz699@gmail.com>
2024-02-05 13:47:29 +0100

    For instance

--- csharp-inheritance/1-instance/1-instance.cs
+++ csharp-inheritance/1-instance/1-instance.cs
@@ -4,9 +4,7 @@ using System.Collections.Generic;
 class Obj
 {
     /// <summary>
-    /// a method that returns 'True' if the object is an instance of,
-    /// or if the object is an instance of a class that inherited 
-    /// from,"Array", otherwise return 'False'.
+    /// a method that returns 'True' if the object is an instance of, or if the object is an instance of a class that inherited from,"Array", otherwise return 'False'.
     /// </summary>
     public static bool IsInstanceOfArray(object obj)
     {
