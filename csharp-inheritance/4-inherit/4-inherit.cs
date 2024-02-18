200fe3c47018b8938975ba245bf77bf3552dc364
azizios011 <hdriaziz699@gmail.com>
2024-02-18 15:11:30 +0100

    It s not a lesson in classes and inheritance without a Dog

---
+++ csharp-inheritance/4-inherit/4-inherit.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+/// <summary>
+/// an empty class 'Animal'.
+/// </summary>
+class Animal
+{
+
+}
+
+/// <summary>
+/// a empty class 'Dog' that inherits from empty class 'Animal'.
+/// </summary>
+class Dog : Animal
+{
+
+}
