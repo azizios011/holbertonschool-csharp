0c48b20a823cb78d0e1363c445ee81c81ff4098d
azizios011 <hdriaziz699@gmail.com>
2024-02-19 16:57:45 +0100

    Basic shapes

---
+++ csharp-inheritance/5-shape/5-shape.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Shape
+{
+    /// <summary>
+    /// public method 'Area' that Throw an 'NotImplementedException' with the message 'Area() is not implemented'.
+    /// </summary>
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
