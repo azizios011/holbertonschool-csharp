38f8e2c6c184390efbbe06658f28a846c75299eb
azizios011 <hdriaziz699@gmail.com>
2024-02-19 20:06:57 +0100

    Type

--- csharp-inheritance/3-type_get/3-type_get.cs
+++ csharp-inheritance/3-type_get/3-type_get.cs
@@ -2,27 +2,27 @@
 using System.Reflection;
 
 /// <summary>
-/// a represention of class "Obj".
+/// A representation of class "Obj".
 /// </summary>
 class Obj
 {
     /// <summary>
-    /// a method that prints the names of the available properties and methods of an object. 
+    /// A method that prints the names of the available properties and methods of an object. 
     /// </summary>
     public static void Print(object myObj)
     {
         TypeInfo typeInfo = myObj.GetType().GetTypeInfo();
 
-        Console.WriteLine($"{typeInfo.Name} Porperties:");
+        Console.WriteLine($"{typeInfo.Name} Properties:");
         foreach (PropertyInfo property in typeInfo.GetProperties())
         {
-            Console.WriteLine(property.Name);
+            Console.WriteLine($"- {property.Name}");
         }
 
         Console.WriteLine($"{typeInfo.Name} Methods:");
         foreach (MethodInfo method in typeInfo.GetMethods())
         {
-            Console.WriteLine(method.Name);
+            Console.WriteLine($"- {method.Name}");
         }
     }
 }
