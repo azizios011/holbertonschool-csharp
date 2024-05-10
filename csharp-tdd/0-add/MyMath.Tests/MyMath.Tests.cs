4a3ad27f3b99b515357e4026bba2d9ac9d6d3988
azizios011 <hdriaziz699@gmail.com>
2024-05-10 20:20:10 +0100

    Add integers

---
+++ csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
@@ -0,0 +1,18 @@
+using System;
+namespace MyMath.Tests {
+
+    /// <summary>
+    /// A test implementation of a class.
+    /// </summary>
+    public class Tests
+    {
+        /// <summary>
+        /// a test method.
+        /// </summary>
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
