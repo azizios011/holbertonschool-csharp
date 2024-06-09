84ac52077344662bf0206e8e71899d44bbb832e4
azizios011 <hdriaziz699@gmail.com>
2024-06-09 04:04:17 +0100

    Divide a matrix

---
+++ csharp-tdd/1-divide/MyMath/MyMath.cs
@@ -0,0 +1,28 @@
+﻿namespace MyMath;
+
+public class Matrix
+{
+    public static int[,] Divide(int[,] matrix, int num)
+    {
+        if (num == 0 && matrix.Length == 0)
+        {
+            throw new DivideByZeroException();
+            return null;
+        }
+        try {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] /= num;
+                }
+            }
+            return matrix;
+        }
+
+        catch (DivideByZeroException)
+        {
+            return null;
+        }
+    }
+}
