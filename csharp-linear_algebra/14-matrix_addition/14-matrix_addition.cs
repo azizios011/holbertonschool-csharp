f2137b45e63fb26aacdf5aec5541e2d8cb44278c
azizios011 <hdriaziz699@gmail.com>
2024-06-13 13:28:54 +0100

    Matrix addition #1

--- csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
+++ csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -1,36 +1,29 @@
-﻿using System;
+﻿﻿using System;
 
-namespace MatrixMath
+/// <summary>
+/// MatrixMath main class.
+/// </summary>
+class MatrixMath
 {
-    public class MatrixMath
-    {
-        public static double[,] Add(double[,] matrix1, double[,] matrix2)
-        {
-            int rows1 = matrix1.GetLength(0);
-            int cols1 = matrix1.GetLength(1);
-            int rows2 = matrix2.GetLength(0);
-            int cols2 = matrix2.GetLength(1);
-
-            // Check if both matrices are either 2x2 or 3x3 and of the same size
-            if ((rows1 != 2 && rows1 != 3) || (cols1 != 2 && cols1 != 3) ||
-                (rows1 != rows2) || (cols1 != cols2))
-            {
-                return new double[,] { { -1 } };
-            }
-
-            // Initialize the resulting matrix
-            double[,] result = new double[rows1, cols1];
-
-            // Add the corresponding elements of the two matrices
-            for (int i = 0; i < rows1; i++)
-            {
-                for (int j = 0; j < cols1; j++)
-                {
-                    result[i, j] = matrix1[i, j] + matrix2[i, j];
-                }
-            }
-
-            return result;
+    /// <summary>
+    /// Method that adds two matrices and returns the resulting matrix.
+    /// </summary>
+    /// <param name="matrix1">First matrix (2D or 3D)</param>
+    /// <param name="matrix2">Second matrix (2D or 3D)</param>
+    /// <returns>returns the resulting matrix.</returns>
+    public static double[,] Add(double[,] matrix1, double[,] matrix2){
+        
+        if (matrix1.Length != matrix2.Length ||
+            matrix1.GetLength(0) != matrix1.GetLength(1) ||
+            matrix2.GetLength(0) != matrix2.GetLength(1)){
+                return (new Double[,]{{-1}});
         }
+        double[,] result = new double[matrix1.GetLength(0),matrix1.GetLength(1)];
+        for (int y = 0; y < matrix1.GetLength(0); y++){
+            for (int x = 0; x < matrix1.GetLength(1); x++){
+                result[y, x] = matrix1[y,x] + matrix2[y,x];
+            }
+        }
+        return result;
     }
 }
