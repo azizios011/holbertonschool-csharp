19da9e5058d36167de6b2566feaf6387dd4721fd
azizios011 <hdriaziz699@gmail.com>
2024-06-26 16:09:45 +0100

    Add integers

--- csharp-tdd/0-add/MyMath/MyMath.cs
+++ csharp-tdd/0-add/MyMath/MyMath.cs
@@ -1,13 +1,7 @@
-﻿using System;
-namespace MyMath {
-    /// <summary>
-    /// A Implementation of class.
-    /// </summary>
+﻿namespace MyMath
+{
     public class Operations
     {
-        /// <summary>
-        /// a method that adds 2 numbers.
-        /// </summary>
         public static int Add(int a, int b)
         {
             return a + b;
