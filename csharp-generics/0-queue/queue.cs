eb9448f75221b427b6ce2e630c6ee714411e4c46
azizios011 <hdriaziz699@gmail.com>
2024-08-11 17:08:33 +0100

    Queue

--- csharp-generics/0-queue/queue.cs
+++ csharp-generics/0-queue/queue.cs
@@ -1,9 +1,19 @@
 ﻿using System;
 
-public class Queue<T>
-{
-    public string CheckType()
+
+    /// <summary>
+    /// Represents a generic queue data structure.
+    /// </summary>
+    /// <typeparam name="T">The type of elements in the queue.</typeparam>
+    public class Queue<T>
     {
-        return typeof(T).FullName;
+        /// <summary>
+        /// Gets the type of the elements in the queue.
+        /// </summary>
+        /// <returns>The fully qualified name of the type parameter <typeparamref name="T"/>.</returns>
+        public string CheckType()
+        {
+            // Returns the type of the generic parameter T
+            return typeof(T).FullName;
+        }
     }
-}
