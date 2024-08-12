4927679f04f1d956a1822f228a7cc2cc9dd38dd3
WhiteWolf <124680639+azizios011@users.noreply.github.com>
2024-08-12 21:37:24 +0100

    User interface

---
+++ csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+/// <summary>
+/// An abstract class called 'Base' containing the following.
+/// </summary>
+public abstract class Base
+{
+    /// <summary>
+    /// Public property 'name'.
+    /// </summary>
+    public string name { get; set; }
+
+    /// <summary>
+    /// Override 'ToString()' method to return the following.
+    /// </summary>
+    public override string ToString()
+    {
+        return $"{name} is a {this.GetType().Name}";
+    }
+}
+
+/// <summary>
+/// An interface called IInteractive.
+/// </summary>
+public interface IInteractive
+{
+    /// <summary>
+    /// Method void Interact()
+    /// </summary>
+    void Interact();
+}
+
+/// <summary>
+/// An interface called IBreakable
+/// </summary>
+public interface IBreakable
+{
+    /// <summary>
+    /// durability should be an int.
+    /// </summary>
+    int durability {  get; set; }
+
+    /// <summary>
+    /// method void Break().
+    /// </summary>
+    void Break();
+}
+
+/// <summary>
+/// An interface called ICollectable.
+/// </summary>
+public interface ICollectable
+{
+    /// <summary>
+    /// isCollected should be a bool/
+    /// </summary>
+    bool isCollected { get; set; }
+
+    /// <summary>
+    /// Method void Collect()
+    /// </summary>
+    void Collect();
+}
+
+/// <summary>
+/// A class called 'TestObject' that inherits from 'Base' and all three interfaces.
+/// </summary>
+public class TestObject : Base, IInteractive, IBreakable, ICollectable
+{
+    public int durability { get; set; }
+
+    public bool isCollected { get; set; }
+    
+    public void Interact()
+    {
+
+    }
+
+    public void Break()
+    {
+
+    }
+
+    public void Collect()
+    {
+
+    }
+
+}
