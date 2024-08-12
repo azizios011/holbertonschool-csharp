9d45626265613690ec23e35de8b7ae70ada6e47c
WhiteWolf <124680639+azizios011@users.noreply.github.com>
2024-08-12 21:15:41 +0100

    Abstract thinking

---
+++ csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+/// <summary>
+/// An abstract class called 'Base' containing the following.
+/// </summary>
+public abstract class Base
+{
+    /// <summary>
+    /// Public property 'name'.
+    /// </summary>
+    public string name { get; set; }
+
+    /// <summary>
+    /// Override 'ToString()' method to return the following.
+    /// </summary>
+    public override string ToString()
+    {
+        return $"{name} is a {this.GetType().Name}";
+    }
+}
