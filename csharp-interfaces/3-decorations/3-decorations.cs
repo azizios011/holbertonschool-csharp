7fe7c941caf03f182c6e8910a49a45a3734a354d
WhiteWolf <124680639+azizios011@users.noreply.github.com>
2024-08-12 22:06:50 +0100

    Interior decorating

---
+++ csharp-interfaces/3-decorations/3-decorations.cs
@@ -0,0 +1,163 @@
+﻿using System;
+
+/// <summary>
+/// An abstract class called 'Base' containing the following.
+/// </summary>
+public abstract class Base
+{
+    /// <summary>
+    /// Public property 'name'.
+    /// </summary>
+    public string name { get; set; }
+
+    /// <summary>
+    /// Override 'ToString()' method to return the following.
+    /// </summary>
+    public override string ToString()
+    {
+        return $"{name} is a {this.GetType().Name}";
+    }
+}
+
+/// <summary>
+/// An interface called IInteractive.
+/// </summary>
+public interface IInteractive
+{
+    /// <summary>
+    /// Method void Interact()
+    /// </summary>
+    void Interact();
+}
+
+/// <summary>
+/// An interface called IBreakable
+/// </summary>
+public interface IBreakable
+{
+    /// <summary>
+    /// durability should be an int.
+    /// </summary>
+    int durability { get; set; }
+
+    /// <summary>
+    /// method void Break().
+    /// </summary>
+    void Break();
+}
+
+/// <summary>
+/// An interface called ICollectable.
+/// </summary>
+public interface ICollectable
+{
+    /// <summary>
+    /// isCollected should be a bool/
+    /// </summary>
+    bool isCollected { get; set; }
+
+    /// <summary>
+    /// Method void Collect()
+    /// </summary>
+    void Collect();
+}
+
+/// <summary>
+/// A class called Door that inherits from 'Base' and 'IInteractive'.
+/// </summary>
+public class Door : Base, IInteractive
+{
+    /// <summary>
+    /// A constructor that sets the value of name.
+    /// </summary>
+    public Door(string name = "Door")
+    {
+        // if 'name' isn’t provided, the default value should be Door.
+        this.name = name;
+    }
+
+    /// <summary>
+    /// implement Interact() so that it prints.
+    /// </summary>
+    public void Interact()
+    {
+        Console.WriteLine($"You try to open the {name}. It's locked.");
+    }
+}
+
+/// <summary>
+/// A class called Decoration that inherits from 'Base', 'IInteractive', and 'IBreakable'.
+/// </summary>
+public class Decoration : Base, IInteractive, IBreakable
+{
+    /// <summary>
+    /// A public bool isQuestItem.
+    /// </summary>
+    public bool isQuestItem { get; set; }
+
+    /// <summary>
+    /// A public bool durability.
+    /// </summary>
+    public int durability { get; set; }
+
+    /// <summary>
+    /// A constructor that sets the value of name, durability, and isQuestItem.
+    /// </summary>
+    public Decoration(string name = "Decoration", int durability = 1, bool isQuestItem = false)
+    {
+        if (durability <= 0)
+        {
+            /// the method declaration should use name, durability, and isQuestItem as the parameter names.
+            throw new Exception("Durability must be greater than 0");
+        }
+
+        this.name = name;
+        this.durability = durability;
+        this.isQuestItem = isQuestItem;
+    }
+
+    /// <summary>
+    /// implement Interact().
+    /// </summary>
+    public void Interact()
+    {
+        /// if the durability is 0 or less, print.
+        if (durability <= 0)
+        {
+            Console.WriteLine($"The {name} has been broken.");
+        }
+        /// otherwise, if isQuestItem is true, print.
+        else if (isQuestItem)
+        {
+            Console.WriteLine($"You look at the {name}. There's a key inside.");
+        }
+        /// if isQuestItem is false, write.
+        else
+        {
+            Console.WriteLine($"You look at the {name}. Not much to see here.");
+        }
+    }
+
+    /// <summary>
+    /// implement Break() so that it decrements durability by 1.
+    /// </summary>
+    public void Break()
+    {
+        durability--;
+        /// if durability is greater than 0, print.
+        if (durability > 0)
+        {
+            Console.WriteLine($"You hit the {name}. It cracks.");
+        }
+        /// if durability is 0, print.
+        else if (durability == 0)
+        {
+            Console.WriteLine($"You smash the {name}. What a mess.");
+        }
+        /// if durability is less than 0, print.
+        else
+        {
+            Console.WriteLine($"The {name} is already broken.");
+        }
+    }
+}
