c8ce782ff7a81616565749da8f879f4560c03379
WhiteWolf <124680639+azizios011@users.noreply.github.com>
2024-08-13 13:33:55 +0100

    Universal health

--- csharp-delegates_events/0-universal_health/0-universal_health.cs
+++ csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -1,28 +1,48 @@
 ﻿using System;
-/// <summary>
-/// public class player 
-/// </summary>
 
-class Player
+/// <summary>
+/// A class called 'Player' with the following.
+/// </summary>
+public class Player
 {
+    /// <summary>
+    /// A Properties 'name' string.
+    /// </summary>
     string name;
+
+    /// <summary>
+    /// A Properties 'maxHp' float.
+    /// </summary>
     float maxHp;
+
+    /// <summary>
+    /// A Properties 'hp' float.
+    /// </summary>
     float hp;
 
+    /// <summary>
+    /// A Constructor.
+    /// </summary>
+    /// <param name="name"></param>
+    /// <param name="maxHp"></param>
     public Player(string name = "Player", float maxHp = 100f)
     {
-        this.name = name;
         if (maxHp <= 0)
         {
             maxHp = 100f;
-            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default..");
         }
-        this.hp = maxHp;
+
+        this.name = name;
         this.maxHp = maxHp;
+        this.hp = maxHp;
     }
 
+    /// <summary>
+    /// A Method.
+    /// </summary>
     public void PrintHealth()
     {
-        Console.WriteLine($"{this.name} has {this.hp} / {this.maxHp} health");
+        Console.WriteLine($"{name} has {hp} / {maxHp} health");
     }
 }
